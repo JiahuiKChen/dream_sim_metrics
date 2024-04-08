db62977a86931bb39166a77aec92aba49ea95e04
JiahuiKChen <jiahui.k.chen@gmail.com>
2024-04-08 17:04:10 -0500

    all method L2 calcs running, real image metrics done

--- similarity_per_class.py
+++ similarity_per_class.py
@@ -1,35 +1,80 @@
+import os 
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"]="3"
+
+import torch
 from dreamsim import dreamsim
 from PIL import Image
-import time
+import torch.nn.functional as F
+from os import listdir
+from tqdm import tqdm
 
 model, preprocess = dreamsim(pretrained=True, cache_dir="/datastor1/jiahuikchen/dreamsim_cache")
 
-start = time.time()
-img1 = preprocess(Image.open("/datastor1/jiahuikchen/synth_ImageNet/embed_cutmix_90/419_76.jpg")).to("cuda")
-img2 = preprocess(Image.open("/datastor1/jiahuikchen/synth_ImageNet/embed_cutmix_90/419_73.jpg")).to("cuda")
+synth_img_root = "/datastor1/jiahuikchen/synth_ImageNet/"
+# synth_img_dirs = [f"{synth_img_root}dropout_90/", f"{synth_img_root}rand_img_cond_90/", 
+#     f"{synth_img_root}cutmix_90/", f"{synth_img_root}mixup_90/", f"{synth_img_root}embed_cutmix_90/",
+#     f"{synth_img_root}embed_mixup_90/", f"{synth_img_root}embed_cutmix_dropout_90/",
+#     f"{synth_img_root}embed_mixup_dropout_90/", f"{synth_img_root}cutmix_dropout_90/", f"{synth_img_root}mixup_dropout_90/"
+# ]
+synth_img_dirs_0 = [f"{synth_img_root}dropout_90/", f"{synth_img_root}rand_img_cond_90/"]
+synth_img_dirs_1 = [f"{synth_img_root}cutmix_90/", f"{synth_img_root}mixup_90/"]
+synth_img_dirs_2 = [f"{synth_img_root}embed_cutmix_90/", f"{synth_img_root}embed_mixup_90/"]
+synth_img_dirs_3 = [f"{synth_img_root}embed_cutmix_dropout_90/", f"{synth_img_root}embed_mixup_dropout_90/"]
+synth_img_dirs_4 = [f"{synth_img_root}cutmix_dropout_90/", f"{synth_img_root}mixup_dropout_90/"]
 
-distance = model(img1, img2)
-end = time.time()
-print(f"Time to preproc + get distance between 2 images: {end - start}")
+real_dirs = ["/data/jiahuic/ImageNetLT_val_test/ImageNet_LT_val_90/", 
+    "/data/jiahuic/ImageNetLT_val_test/ImageNet_LT_test_90/"
+]
 
-start = time.time()
-embedding = model.embed(img1)
-end = time.time()
-print(f"Time to embed 1 image: {end - start}")
 
-start = time.time()
-img1 = preprocess(Image.open("/datastor1/jiahuikchen/synth_ImageNet/embed_cutmix_90/419_76.jpg")).to("cuda")
-end = time.time()
-print(f"Time to preproc 1 image: {end - start}")
+# given a directory of images, generates dreamsim embeddings for all images
+# returns list of embeddings 
+def gen_all_embeddings(img_dir):
+    embeddings = []
+    print(f"Creating embeddings for: {img_dir.split('/')[-2]}")
 
-#example use:
-# Perceptual similarity metric
-# img1 = preprocess(Image.open("img1_path"))
-# img2 = preprocess(Image.open("img2_path"))
+    for file in tqdm(listdir(img_dir)):
+        img_path = f"{img_dir}{file}"
+        embeddings.append(model.embed(preprocess(Image.open(img_path)).to("cuda")))
 
-# distance = model(img1, img2)
+    torch.save(embeddings, f"{img_dir.split('/')[-2]}.pt")
+    return embeddings
 
-# TODO: USE THEIR "FASTER" CODE: https://github.com/ssundaram21/dreamsim?tab=readme-ov-file#image-retrieval 
+
+# given a list of embeddings compute average pairwise distance 
+# between all possible pairs 
+def avg_L2_distance(embeddings):
+    dist_total = 0
+    pair_count = 0
+
+    for i, embed in tqdm(enumerate(embeddings), total=len(embeddings)):
+        for j in range(len(embeddings)):
+            if i == j:
+                continue
+            other_embed = embeddings[j]
+            dist = F.pairwise_distance(embed, other_embed)
+            dist_total += dist
+            pair_count += 1
+    
+    avg_dist = dist_total / pair_count
+    return avg_dist
+
+
+def calc_L2_pairwise(img_dirs, out_file_name):
+    out_str = ""
+
+    for dir in img_dirs:
+        dir_name = dir.split('/')[-2]
+        embeddings = gen_all_embeddings(dir)
+        avg_dist = avg_L2_distance(embeddings)
+
+        dist_str = f"{dir_name}\t{avg_dist}\n"
+        print(dist_str)
+        out_str += dist_str
+
+    with open(out_file_name, "w") as out_file:
+        out_file.write(out_str)
 
 # TODO: 
 # - for each 90 class image gen method: 
@@ -39,3 +84,9 @@ print(f"Time to preproc 1 image: {end - start}")
 #       do the same thing 
 # ideally: we see average pairwise distance between synthetic images is higher than real images
 # cause then we have introduced "good" diversity (for methods where accuracy goes up notably)
+
+# all synthetic image 90 sets L2 
+calc_L2_pairwise(synth_img_dirs_4, "avg_pairwise_dists_synth_4.txt")
+
+# real validation and test set L2
+# calc_L2_pairwise(real_dirs, "avg_pairwise_dists_real.txt")
